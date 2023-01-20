cc0f78d085135d9d5d3fb2bb7c780201138a4fe1
Juan Bezanilla <juanbezanillaz@gmail.com>
2023-01-20 12:37:11 -0300

    Crud Complete

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,42 @@
+class VideosController < ApplicationController
+
+  def index
+    @videos = Video.all
+  end
+
+  def new
+    @video = Video.new
+  end
+
+  def show
+    @video = Video.find(params[:id])
+  end
+
+  def create
+    @video = Video.new(video_params)
+    @video.save
+    redirect_to video_path(@video)
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    @video.update(video_params)
+    redirect_to video_path(@video)
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to videos_path, status: :see_other
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :category, :published_year)
+  end
+end
